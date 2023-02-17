98d4b93e2c3fb1ad87ad56471542de194b350110
Bobson Prosper Paebi <bobsonconnect@gmail.com>
2023-02-17 15:16:40 -0800

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,16 +1,15 @@
-
 #include <stdio.h>
 
 /**
- * main - prints the alphabet in lowercase.
+ * main - Prints the alphabet in lowercase.
  *
  * Return: Returns 0.
  */
 int main(void)
 {
 	char letters[26] = "abcdefghijklmnopqrstuvwxyz";
-	
 	int i;
+
 	for (i = 0; i < 26; i++)
 	{
 		putchar(letters[i]);
