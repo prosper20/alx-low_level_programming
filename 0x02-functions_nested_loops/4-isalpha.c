e5a72324cc888ca05db87a6e733a2d63c7f5ab42
Bobson Prosper Paebi <bobsonconnect@gmail.com>
2023-02-21 14:15:47 -0800

    Added the file 4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _isalpha - Checks for alphabetic character.
+ * @c: The character to be checked
+ *
+ * Return: 1 if it's a letter. 0 otherwise.
+ */
+int _isalpha(int c)
+{
+	return (((c >= 65 && c <= 90) || (c >= 97 && c <= 122)) ? 1 : 0);
+}
