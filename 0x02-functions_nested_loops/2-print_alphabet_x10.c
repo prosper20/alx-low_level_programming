89b40d474a84beb4dc23eccb858d052a448cd4e8
Bobson Prosper Paebi <bobsonconnect@gmail.com>
2023-02-21 10:37:43 -0800

    Updated the file 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_alphabet - Prints 10 times the alphabet, in lowercase.
+ * print_alphabet_x10 - Prints 10 times the alphabet, in lowercase.
  *
  * Return: Returns void.
  */
