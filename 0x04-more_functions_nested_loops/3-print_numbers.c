07d577beea87ef10661abdb5b6227dae80b19791
Bobson Prosper Paebi <bobsonconnect@gmail.com>
2023-02-23 09:23:53 -0800

    Added the file 3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - Prints the numbers 0 to 9.
+ *
+ * Return: Returns void
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
