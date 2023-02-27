80e9044a1f61a71a0830e20d7345c61d6728d006
Bobson Prosper Paebi <bobsonconnect@gmail.com>
2023-02-27 15:32:29 -0800

    Added the file 6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * puts2 - Skips one character before printing the next.
+ * @str: Input string.
+ * Return: Returns void.
+ */
+void puts2(char *str)
+{
+	int count = 0;
+
+	while (count >= 0)
+	{
+		if (str[count] == '\0')
+		{
+			_putchar('\n');
+			break;
+		}
+		if (count % 2 == 0)
+			_putchar(str[count]);
+		count++;
+	}
+}
